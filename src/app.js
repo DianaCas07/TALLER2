63511baad70d76170cc32ce031fa139a70e9fb3b
DianaCas07 <00036625@uca.edu.sv>
2026-09-10 15:37:43 -0600

    Revert "add: subir avance de ejercicio 2"
    This reverts commit 07a57c7df2093470eca22356299019fa3a258627, reversing
    changes made to 91ef0e1d4008b03ca9afec9977f8917e64f034d1.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
 const port = 3000
 
 app.use(express.json());
@@ -77,35 +76,3 @@ const calcularCosto = (pais, peso) =>{
 app.listen(port, () => {
     console.log(`Servidor ejecutándose en http://localhost:${port}`);
 });
-=======
-const port = 3157;
-
-app.get('/api/ivarenta/:monto', (req, res) =>{
-    const monto = Number(req.params.monto);
-
-    try {
-        if (req.params.monto <0 || req.params.monto ==0)
-    {
-        return res.status(500).json({error: 'El salario debe ser un numero mayor a 0'})
-    }	
-
-const iva = monto * 0.13;
-const renta = monto * 0.10;
-const resObj = {
-	montoOriginal: monto,
-ivaCalculado: iva,
-rentaCalculada: renta,	
-};
-
-res.json(resObj);
-} catch(error){
-	res.status(500).json({error: 'Salario invalid o suceido un error en el calculo'});
-}
-
-});
-
-app.listen(port, () =>{
-	console.log(`Servidor escuchando en http://localhost:${port}`);
-});
-
->>>>>>> 03d5d8d00f4d7abdd26a0831f582a0ac1de72f42
